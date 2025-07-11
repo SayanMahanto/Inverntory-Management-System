432cdc8c47d23fe4f04a9b591e2b8ece2f2242ad
Sayan <mahantosayan@gmail.com>
2025-07-11 14:24:38 +0530

    Auth controller changes

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -17,7 +17,7 @@ export const registerUser = async (req, res) => {
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser)
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: "User already exists!" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
@@ -27,7 +27,9 @@ export const registerUser = async (req, res) => {
       role,
     });
 
-    return res.status(200).json({ message: "User created successfully" });
+    return res
+      .status(200)
+      .json({ user: user, message: "User created successfully." });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
